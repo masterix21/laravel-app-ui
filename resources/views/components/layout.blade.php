030d5fdd3daf2eb5d4846e737b2ab132e4d34311
Luca Longo <masterix21@gmail.com>
2021-11-02 17:30:43 +0100

    wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,11 @@
             <div class="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
                 <x-app-ui::sidebar />
 
-                <main @class(["lg:col-span-9" => true, "xl:col-span-6" => $aside ?? false])>
+                <main
+                    @class([
+                        "lg:col-span-9 xl:col-span-10" => true,
+                        "xl:col-span-6" => $aside ?? false
+                    ])>
                     {{ $slot ?? '' }}
                 </main>
 
